9b0afa72b37c60d652e80c233908b751b75baf72
matti.rigo@gmail.com <matti.rigo@gmail.com>
2019-05-17 17:26:02 +0200

    Missing modules included. New training queries

---
+++ SQLiteUtils/Model/Wrappers/TableWrappers/PersonalRecordWrapper.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteUtils.Model
+{
+    public class PersonalRecordWrapper : DatabaseObjectWrapper
+    {
+
+
+        #region Consts
+        private const string DefaultTableName = "PersonalRecord";
+        #endregion
+
+
+        #region Private Fields
+        private int _userIdMin;
+        private int _userIdMax;
+        private int _excerciseIdMin;
+        private int _excerciseIdMax;
+        #endregion
+
+
+        #region Properties
+        /// <summary>
+        /// Specific date for the entry, random otherwise.
+        /// </summary>
+        public DateTime RecordDate { get; set; } = DatabaseUtility.UnixTimestampT0;
+
+        /// <summary>
+        /// Specific User Id for the entry, random otherwise.
+        /// </summary>
+        public long UserId { get; set; } = 0;
+
+        /// <summary>
+        /// Excercise Id for the entry, random otherwise.
+        /// </summary>
+        public long ExcerciseId { get; set; } = 0;
+        #endregion
+
+
+        #region Ctor
+        /// <summary>
+        /// Wrapper for the Post DB table.
+        /// </summary>
+        /// <param name="connection"></param>
+        public PersonalRecordWrapper(SQLiteConnection connection) : base(connection, DefaultTableName, true)
+        {
+            _userIdMin = 1;
+            _userIdMax = DatabaseUtility.GetTableMaxId(connection, "User", true);
+
+            _excerciseIdMin = 1;
+            _excerciseIdMax = DatabaseUtility.GetTableMaxId(connection, "Excercise", true);
+        }
+
+
+        /// <summary>
+        /// Wrapper for the Post DB table.
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="userIdMin">Lowest userId ffrom the User table</param>
+        /// <param name="userIdMax">Highest userId ffrom the User table</param>
+        public PersonalRecordWrapper(SQLiteConnection connection, int userIdMin, int userIdMax) : base(connection, DefaultTableName, true)
+        {
+            _userIdMin = userIdMin;
+            _userIdMax = userIdMax;
+        }
+        #endregion
+
+
+        #region Override Methods
+        /// <summary>
+        /// Generates an entry with random but meaningful values. DB Integreity is ensured.
+        /// <param name="parentId">Id, otherwise it will be automatically-generated</param>
+        /// </summary>
+        public override List<DatabaseColumnWrapper> Create(long parentId = 0)
+        {
+
+            // Parse columns and generate the fields
+            foreach (DatabaseColumnWrapper col in Entry)
+            {
+                switch (col.Name)
+                {
+
+
+                    case "Value":
+
+                        col.Value = RandomFieldGenerator.RandomInt(50, 1200);
+                        break;
+
+                    case "RecordDate":
+
+                        if (RecordDate == DatabaseUtility.UnixTimestampT0)
+                            col.Value = RandomFieldGenerator.RandomUnixTimestamp(GymAppSQLiteConfig.DbDateLowerBound, GymAppSQLiteConfig.DbDateUpperBound);
+                        else
+                            col.Value = DatabaseUtility.GetUnixTimestamp(RecordDate);
+                        break;
+
+                    case "ExcerciseId":
+
+                        if (ExcerciseId == 0)
+                            col.Value = RandomFieldGenerator.RandomInt(_excerciseIdMin, _excerciseIdMax + 1);
+                        else
+                            col.Value = ExcerciseId;
+
+                        break;
+
+                    case "UserId":
+
+                        if (UserId == 0)
+                            col.Value = RandomFieldGenerator.RandomInt(_userIdMin, _userIdMax + 1);
+                        else
+                            col.Value = UserId;
+
+                        break;
+
+                    default:
+
+                        col.Value = RandomFieldGenerator.GenerateRandomField(col);
+                        break;
+                }
+            }
+            RecordDate = DatabaseUtility.UnixTimestampT0;
+            UserId = 0;
+            ExcerciseId = 0;
+
+            // Create new ID
+            try
+            {
+                checked { MaxId++; };
+            }
+            catch (OverflowException)
+            {
+                return null;
+            }
+
+
+            // New entry processed
+            GeneratedEntryNumber++;
+
+            return Entry;
+        }
+        #endregion
+    }
+}
---
+++ SQLiteUtils/Util/SQLiteGymAppFunctions.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SQLiteUtils.Util
+{
+
+    public static class SQLiteGymAppFunctions
+    {
+
+
+
+
+        [SQLiteFunction(Name = "RmToIntensityPerc", Arguments = 1, FuncType = FunctionType.Scalar)]
+        public class RmToIntensityPercSQLiteFunction : SQLiteFunction
+        {
+
+            public override object Invoke(object[] args)
+            {
+                return 0.4167 * Convert.ToInt32(args[0]) - 14.2831 * Math.Pow(Convert.ToInt32(args[0]), 0.5) + 115.6122;
+            }
+        }
+
+
+
+        [SQLiteFunction(Name = "IntensityPercToRm", Arguments = 1, FuncType = FunctionType.Scalar)]
+        public class IntensityPercToRmSQLiteFunction : SQLiteFunction
+        {
+
+            public override object Invoke(object[] args)
+            {
+                return 324.206809067032 - 18.0137586362208 * Convert.ToDouble(args[0]) + 0.722425494099458 * Math.Pow(Convert.ToDouble(args[0]), 2) - 0.018674659779516 * Math.Pow(Convert.ToDouble(args[0]), 3) + 0.00025787003728422 * Math.Pow(Convert.ToDouble(args[0]), 04)
+                    - 1.65095582844966E-06 * Math.Pow(Convert.ToDouble(args[0]), 5) + 2.75225269851 * Math.Pow(10, -9) * Math.Pow(Convert.ToDouble(args[0]), 6) + 8.99097867 * Math.Pow(10, -12) * Math.Pow(Convert.ToDouble(args[0]), 7);
+            }
+        }
+
+
+
+
+        [SQLiteFunction(Name = "EffortToRpe", Arguments = 3, FuncType = FunctionType.Scalar)]
+        public class EffortToRpeSQLiteFunction : SQLiteFunction
+        {
+
+            // TCL
+            //set effort[lindex $argv 0]
+            //set effortType[lindex $argv 1]
+            //set targetReps[lindex $argv 2]
+
+            //if {$effortType == 1} {
+            //      set intensity[expr { $effort / 10.0 }]
+            //      set rm[expr { 324.206809067032 - 18.0137586362208 * $intensity + 0.722425494099458 * pow($intensity, 2) - 0.018674659779516 * pow($intensity, 3) + 0.00025787003728422 * pow($intensity, 04) - 1.65095582844966E-06 * pow($intensity, 5) + 2.75225269851 * pow(10, -9) * pow($intensity, 6) + 8.99097867 * pow(10, -12) * pow($intensity, 7) }]
+            //      set val[expr { 10 - ($rm - $targetReps) }]
+            //      set retval[expr round($val)]
+
+            //} elseif {$effortType == 2} {
+            //   set retval[expr { 10 - ($effort - $targetReps) }]
+            //} elseif {$effortType == 3} {
+            //   return $effort
+            //} else {
+            //   return 0
+            //}	
+
+            //if {$retval > 4} {
+            //   return $retval 
+            //} else {
+            //   return 4
+            //}
+
+            public override object Invoke(object[] args)
+            {
+                try
+                {
+                    int effort = Convert.ToInt32(args[0]);
+                    int effortType = Convert.ToInt32(args[1]);
+                    int targetReps = Convert.ToInt32(args[2]);
+
+
+                    switch (effortType)
+                    {
+
+                        case 1:
+
+                            // Intensity
+                            double rm = 0.4167 * effort / 10.0f - 14.2831 * Math.Pow(effort / 10.0f, 0.5) + 115.6122;
+                            return Math.Max(Math.Round(10 - (rm - targetReps), 0), 4);
+
+                        case 2:
+
+                            // Rm
+                            return Math.Max(10 - (effort - targetReps), 4);
+
+                        case 3:
+
+                            // RPE
+                            return effort;
+
+                        default:
+                            return 0;
+
+                    }
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+
+
+
+
+        [SQLiteFunction(Name = "EffortToIntensityPerc", Arguments = 3, FuncType = FunctionType.Scalar)]
+        public class EffortToIntensityPercSQLiteFunction : SQLiteFunction
+        {
+
+            // TCL
+            //set effort[lindex $argv 0]
+            //set effortType[lindex $argv 1]
+            //set targetReps[lindex $argv 2]
+
+            //if {$effortType == 1} {
+
+            //   expr { $effort / 10.0 }
+
+            //} elseif {$effortType == 2} {
+
+            //      set intensity[expr { 0.4167 * $effort - 14.2831 * pow($effort, 0.5) + 115.6122 }]
+            //      expr round($intensity* 10.0) / 10.0
+
+            //} elseif {$effortType == 3} {
+
+            //      set param[expr { $targetReps + (10 - $effort) }]
+            //      set intensity[expr { 0.4167 * $param - 14.2831 * pow($param, 0.5) + 115.6122 }]
+            //      expr round($intensity)
+
+            //} else {
+            //   return 0
+            //}	
+
+            public override object Invoke(object[] args)
+            {
+                try
+                {
+                    int effort = Convert.ToInt32(args[0]);
+                    int effortType = Convert.ToInt32(args[1]);
+                    int targetReps = Convert.ToInt32(args[2]);
+
+                    switch (effortType)
+                    {
+
+                        case 1:
+
+                            // Intensity
+                            return effort / 10.0;
+
+                        case 2:
+
+                            // Rm
+                            return Math.Round(0.4167 * effort - 14.2831 * Math.Pow(effort, 0.5) + 115.6122, 1);
+
+                        case 3:
+
+                            // RPE
+                            int rm = targetReps + (10 - effort);
+                            return Math.Round(0.4167 * rm - 14.2831 * Math.Pow(rm, 0.5) + 115.6122, 1);
+
+                        default:
+                            return 0;
+
+                    }
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
